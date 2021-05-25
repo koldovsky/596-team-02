fe2b6083d692694810fbcca29308bd1e54688da3
ruslan-teh <83656149+ruslan-teh@users.noreply.github.com>
2021-05-25 20:15:13 +0300

    html code

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const startTime = new Date();
+const maxTimeSecs = 30000;
+
+function updateClock() {
+    const clockContainer = document.querySelector('.clock');
+    const deltaSecs = Math.round((new Date() - startTime) / 1000);
+    const secsLeft = maxTimeSecs - deltaSecs;
+    clockContainer.innerText = secsLeft;
+}
+
+setInterval(updateClock, 1000);
